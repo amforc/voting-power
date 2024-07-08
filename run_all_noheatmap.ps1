0f103e7b03f806b180c090905c7464061f5cb94f
RaffiHu <115583622+RaffiHu@users.noreply.github.com>
2024-07-08 11:21:45 +0200

    add DV Cohort 2 run

--- run_all_noheatmap.ps1
+++ run_all_noheatmap.ps1
@@ -9,10 +9,7 @@ Write-Host "Date: $date, Network: $network, Lookback: $lookback"
 $commands = @(
     "python .\voting_from_subscan.py $network $lookback",
     "python .\prepare_data.py ${date}_${network}_${lookback}_days.csv",
-    ".\run_banzhaf.ps1",
-    "python .\heatmap_prepare_data.py ${date}_${network}_${lookback}_days.csv",
-    ".\run_banzhaf_heatmap.ps1",
-    ".\run_banzhaf_heatmap_plots.ps1"
+    ".\run_banzhaf.ps1"
 )
 
 foreach ($command in $commands) {
